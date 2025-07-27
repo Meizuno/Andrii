aefac12a53b17c77fdbd675ab90221d553c22357
Yurii Myronov <yuramiron16@gmail.com>
2025-07-27 07:38:01 +0200

    fix layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-dvh grid grid-rows-[auto_1fr] gap-4">
     <AppHeader />
-    <div class="flex flex-col gap-4 overflow-auto">
+    <div class="h-full flex flex-col gap-4 overflow-auto justify-between">
       <UContainer as="main">
         <slot />
       </UContainer>
