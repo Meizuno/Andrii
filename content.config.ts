0df1f4501a13b6aa2f8fd0e57ca1060934ebc5d7
Yurii Myronov <yuramiron16@gmail.com>
2025-07-27 08:02:06 +0200

    add content sitemap

---
+++ content.config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, defineContentConfig } from '@nuxt/content'
+import { asSitemapCollection } from '@nuxtjs/sitemap/content'
+
+export default defineContentConfig({
+  collections: {
+    content: defineCollection(
+      asSitemapCollection({
+        type: 'page',
+        source: '**/*.md',
+      }),
+    ),
+  },
+})
