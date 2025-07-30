b86b1822d04845abafb7053cf26e671ee0264984
Yurii Myronov <yuramiron16@gmail.com>
2025-07-30 17:58:44 +0200

    redirect invalid page to home

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,12 +1,16 @@
 <script lang="ts" setup>
-const route = useRoute()
+const route = useRoute();
 const { data: page } = await useAsyncData(route.path, () => {
-  return queryCollection('content').path(route.path).first()
-})
+  return queryCollection("content").path(route.path).first();
+});
+
+if (!page.value) {
+  navigateTo("/");
+}
 
 useHead({
   titleTemplate: (titleChunk) => {
-    let title = ""
+    let title = "";
     if (titleChunk) {
       title = titleChunk;
     }
@@ -14,7 +18,7 @@ useHead({
     if (page.value && page.value.title) {
       title += ` | ${page.value.title}`;
     }
-    return title
+    return title;
   },
 });
 </script>
@@ -22,12 +26,5 @@ useHead({
 <template>
   <div v-if="page" class="h-full">
     <ContentRenderer :value="page" />
-  </div>
-  <div v-else>
-    <div class="h-full">
-      <h1>Page Not Found</h1>
-      <p>Oops! The content you're looking for doesn't exist.</p>
-      <NuxtLink to="/">Go back home</NuxtLink>
-    </div>
   </div>
 </template>
